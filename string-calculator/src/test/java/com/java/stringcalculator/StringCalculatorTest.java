6e1351b00839f8d3618301885730c18b7fce9075
Ashutosh Dubey <ashutoshdubey899@gmail.com>
2021-03-07 15:56:54 +0530

    Test -2 Unknown amount of Inputs

--- string-calculator/src/test/java/com/java/stringcalculator/StringCalculatorTest.java
+++ string-calculator/src/test/java/com/java/stringcalculator/StringCalculatorTest.java
@@ -18,7 +18,7 @@ public class StringCalculatorTest {
 	public void initialize() {
 		calculator = new StringCalculator();
 	}
-
+/// Test -1 Basic Test
 	@Test
 	public void emptyStringShouldReturn0() {
 		assertEquals(calculator.add(""), 0);
@@ -35,4 +35,11 @@ public class StringCalculatorTest {
 		assertEquals(calculator.add("1,2"), 3);
 		assertEquals(6, calculator.add("1,5"));
 	}
+	
+//Test -2 Unknown amount of numbers
+	 @Test
+	    public void handleMultipleInput() {
+		 assertEquals(45, calculator.add("1,2,3,4,5,6,7,8,9"));
+	        
+	    }
 }
